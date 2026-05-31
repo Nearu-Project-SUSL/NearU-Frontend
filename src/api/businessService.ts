94595c00ad6d81367f1ed8acaa10c1cfe0ba467e
pamal pahasara <pamalpahasara@gmail.com>
2026-05-31 20:25:19 +0530

    pop for fill up questions to complete profile

--- src/api/businessService.ts
+++ src/api/businessService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5059/api';
+const API_BASE = import.meta.env.VITE_API_BASE_URL || 'https://api.nearusab.me/api';
 
 const getAuthHeader = () => ({
   headers:{
